8844a9ccb5c00b8800e02f5822d1d24a18262a04
lnamur <lnamur@pm.me>
2026-04-15 12:28:30 +0200

    delete button basket

--- basket.js
+++ basket.js
@@ -1,6 +1,12 @@
+fetch('./header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-container').innerHTML = data;
+    });
+
 const purchaseDiv = document.getElementById('purchasse')
 
-function createTripDiv(trip, divParent){
+function createTripDiv(trip, divParent) {
     const tripDiv = document.createElement('div')
 
     tripDiv.classList.add('trips')
@@ -10,8 +16,9 @@ function createTripDiv(trip, divParent){
     tripDiv.style.alignItems = 'center'
     tripDiv.style.justifyContent = 'space-evenly'
     tripDiv.style.gap = '5%'
-    tripDiv.style.width = '100%'  
-    tripDiv.style.height = '20%' 
+    tripDiv.style.width = '100%'
+    tripDiv.style.textAlign = 'center'
+    tripDiv.style.height = '20%'
     tripDiv.style.borderRadius = '5px'
     tripDiv.style.boxShadow = '1px 1px 1px 1px rgb(0 0 0 / 20%)'
 
@@ -20,25 +27,41 @@ function createTripDiv(trip, divParent){
     createSpan('hour', tripDiv, trip.date)
     createSpan('price', tripDiv, `${trip.price}€`)
 
-    createDeleteButton(tripDiv)
+    async function handleEvent() {
+        await fetch('http://localhost:3000/baskets/delete/trip', {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ trip: trip['_id'] })
+        })
+
+        tripDiv.remove()
+    }
+
+    createDeleteButton(tripDiv, handleEvent)
 
     divParent.appendChild(tripDiv)
 }
 
-function createSpan(spanClass, div, content){
+function createSpan(spanClass, div, content) {
     const span = document.createElement('span')
     span.classList.add(spanClass)
+    span.style.width = '20%'
     span.textContent = content
     div.appendChild(span)
 }
 
-function createDeleteButton(div){
+function createDeleteButton(div, handleEvent) {
     const deleteButton = document.createElement('button')
-    deleteButton.textContent = 'X' 
+    deleteButton.classList.add('deleteSpanButton')
+    deleteButton.textContent = 'X'
+    deleteButton.addEventListener('click', handleEvent)
     div.appendChild(deleteButton)
 }
 
-async function addBasketTrip(){
+async function addBasketTrip() {
+    const idTripArray = []
     const response = await fetch('http://localhost:3000/baskets')
     const jsonResponse = await response.json()
 
@@ -46,14 +69,15 @@ async function addBasketTrip(){
     tripsDiv.style.display = 'flex'
     tripsDiv.style.flexDirection = 'column'
     tripsDiv.style.justifyContent = 'center'
-    tripsDiv.style.gap = '5px'
+    tripsDiv.style.gap = '10px'
     tripsDiv.style.height = '70%'
     tripsDiv.style.padding = '10px'
 
-    for(const response of jsonResponse){   
-        const trip =  response.trip 
+    for (const response of jsonResponse) {
+        const trip = response.trip
         createTripDiv(trip, tripsDiv)
     }
+    return idTripArray
 }
 
 addBasketTrip()
