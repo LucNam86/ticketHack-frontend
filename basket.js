ac362f908b12d54add2cf7ada171eb20a9fc2935
lnamur <lnamur@pm.me>
2026-04-15 11:21:15 +0200

    basket js

--- basket.js
+++ basket.js
@@ -1,6 +1,6 @@
 const purchaseDiv = document.getElementById('purchasse')
 
-function createTripDiv(trip){
+function createTripDiv(trip, divParent){
     const tripDiv = document.createElement('div')
 
     tripDiv.classList.add('trips')
@@ -9,21 +9,26 @@ function createTripDiv(trip){
     tripDiv.style.display = 'flex'
     tripDiv.style.alignItems = 'center'
     tripDiv.style.justifyContent = 'space-evenly'
-    tripDiv.style.gap = '5%'   
+    tripDiv.style.gap = '5%'
+    tripDiv.style.width = '100%'  
+    tripDiv.style.height = '20%' 
+    tripDiv.style.borderRadius = '5px'
+    tripDiv.style.boxShadow = '1px 1px 1px 1px rgb(0 0 0 / 20%)'
 
     const spanTravelContent = `${trip.departure} > ${trip.arrival}`
     createSpan('travel', tripDiv, spanTravelContent)
     createSpan('hour', tripDiv, trip.date)
-    createSpan('price', tripDiv, trip.price)
+    createSpan('price', tripDiv, `${trip.price}€`)
 
     createDeleteButton(tripDiv)
 
+    divParent.appendChild(tripDiv)
 }
 
 function createSpan(spanClass, div, content){
-    const span= document.createElement('span')
+    const span = document.createElement('span')
     span.classList.add(spanClass)
-    spanTravel.textContent = content
+    span.textContent = content
     div.appendChild(span)
 }
 
@@ -34,8 +39,21 @@ function createDeleteButton(div){
 }
 
 async function addBasketTrip(){
-    const trips = await fetch('http://localhost:3000/baskets')
-    for(const trip of trips){     
-        createTripDiv(trip)
+    const response = await fetch('http://localhost:3000/baskets')
+    const jsonResponse = await response.json()
+
+    const tripsDiv = document.getElementById('trips')
+    tripsDiv.style.display = 'flex'
+    tripsDiv.style.flexDirection = 'column'
+    tripsDiv.style.justifyContent = 'center'
+    tripsDiv.style.gap = '5px'
+    tripsDiv.style.height = '70%'
+    tripsDiv.style.padding = '10px'
+
+    for(const response of jsonResponse){   
+        const trip =  response.trip 
+        createTripDiv(trip, tripsDiv)
     }
 }
+
+addBasketTrip()
